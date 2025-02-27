7bafdd6d24e40e12a7b9cb640eacfe362f5a0ff7
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-02-27 10:56:01 +0530

    app,main.jsx files imporve and imports

--- chat-app/src/App.jsx
+++ chat-app/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login/Login'
+import Chat from './pages/Chat/Chat'
+import ProfileUpdate from './pages/ProfileUpdate/ProfileUpdate'
 
 const App = () => {
   return (
-    <div>
-
-    </div>
+    <>
+      <Route>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/chat' element={<Chat/>}/>
+        <Route path='/profile' element={<ProfileUpdate/>}/>        
+      </Route>
+    </>
   )
 }
 
