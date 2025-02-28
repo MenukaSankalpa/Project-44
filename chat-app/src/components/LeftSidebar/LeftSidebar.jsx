8eda0cbdb55b0c2a08c2beb2d01fcafee53f6648
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-02-28 10:44:41 +0530

    chat improve

--- chat-app/src/components/LeftSidebar/LeftSidebar.jsx
+++ chat-app/src/components/LeftSidebar/LeftSidebar.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './LeftSidebar.css'
+import assets from '../../assets/assets'
 
 const LeftSidebar = () => {
   return (
-    <div>
-
+    <div className='ls'>
+      <div className="ls-stop">
+        <div className="ls-nav">
+          <img src={assets.logo} className='logo' alt="" />
+          <div className="menu">
+              <img src={assets.menu_icon} alt="" />
+          </div>   
+        </div>
+        <div className="ls-search">
+          <img src={assets.search_icon} alt="" />
+          <input type="text" placeholder='Search here...' />
+        </div>
+      </div>
     </div>
   )
 }
