0e6c3362fc8b4bddf70bb7a946b996db7e2c93de
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-02-28 09:29:42 +0530

    update login.jsx

--- chat-app/src/pages/Login/Login.jsx
+++ chat-app/src/pages/Login/Login.jsx
@@ -11,16 +11,21 @@ const Login = () => {
         <img src={assets.logo_big} alt="" className="logo"/> 
         <form  className="login-form">
             <h2>{currState}</h2>
-            <input type="text" placeholder='username' className="form-input" required />
+            {currState === "Sign up"?<input type="text" placeholder='username' className="form-input" required />:null}
             <input type="text" placeholder='Email address' className="form-input" />
             <input type="text" placeholder='password' className="form-input" />
-            <button type='submit'>Sign Up</button>
+            <button type='submit'>{currState === "Sign up"?"Create account":"Login now"}</button>
             <div className="login-term">
                 <input type="checkbox"/>
                 <p>Agree to the terms of use & privacy policy.</p>
             </div>
             <div className="login-forgot">
-                <p className="login-toggle">Already have an account <span onClick={()=>setCurrState("Login")}>click here</span> </p>
+                {
+                    currState === "Sign up"
+                        ?<p className="login-toggle">Already have an account <span onClick={()=>setCurrState("Login")}>Login here</span> </p>
+                        :<p className="login-toggle">Create an account <span onClick={()=>setCurrState("Sign up")}>click here</span> </p>
+                }
+                
             </div>
         </form>
     </div>
