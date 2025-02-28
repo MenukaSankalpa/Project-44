e41791710e3f1bff70165beddfb41061c7edd8dd
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-02-28 08:59:34 +0530

    set function to click here and display login

--- chat-app/src/pages/Login/Login.jsx
+++ chat-app/src/pages/Login/Login.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import assets  from '../../assets/assets'
 
 const Login = () => {
+
+    const [currState,setCurrState] = useState("Sign up");
+
   return (
     <div className='login'>
         <img src={assets.logo_big} alt="" className="logo"/> 
         <form  className="login-form">
-            <h2>Sign Up</h2>
+            <h2>{currState}</h2>
             <input type="text" placeholder='username' className="form-input" required />
             <input type="text" placeholder='Email address' className="form-input" />
             <input type="text" placeholder='password' className="form-input" />
@@ -17,7 +20,7 @@ const Login = () => {
                 <p>Agree to the terms of use & privacy policy.</p>
             </div>
             <div className="login-forgot">
-                <p className="login-toggle">Already have an account <span>click here</span> </p>
+                <p className="login-toggle">Already have an account <span onClick={()=>setCurrState("Login")}>click here</span> </p>
             </div>
         </form>
     </div>
