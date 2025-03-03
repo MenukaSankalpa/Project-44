2d35aa3a2b577d35ca4a170acb15cc446bd2c888
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-03-03 10:06:06 +0530

    chat-msg updated in css

--- chat-app/src/components/ChatBox/ChatBox.jsx
+++ chat-app/src/components/ChatBox/ChatBox.jsx
@@ -11,7 +11,24 @@ const ChatBox = () => {
             <img src={assets.help_icon} className='help' alt="" />
         </div>
 
-        
+        <div className="chat-msg">
+            <div className="s-msg">
+                <p className="msg">Do all assignment before the deadline...</p>
+                <div>
+                    <img src={assets.profile_img} alt="" />
+                    <p>2.30 PM</p>
+                </div>
+            </div>
+            <div className="r-msg">
+                <p className="msg">Do all assignment before the deadline...</p>
+                <div>
+                    <img src={assets.profile_img} alt="" />
+                    <p>2.30 PM</p>
+                </div>
+            </div>
+        </div>
+
+
         <div className="chat-input">
             <input type="text" placeholder='Send aMessage...'/>
             <input type="file" id='image' accept='image/png, image/jpeg' hidden />
