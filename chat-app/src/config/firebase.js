c0baea9a8c25f3d49a878d91ad6dfdddc381825e
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-03-05 16:53:32 +0530

    add firebase documents

---
+++ chat-app/src/config/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCRSvq_NOfPeg3JgmFhGCPT7_abpT1gCRs",
+  authDomain: "chat-app-gs-54113.firebaseapp.com",
+  projectId: "chat-app-gs-54113",
+  storageBucket: "chat-app-gs-54113.firebasestorage.app",
+  messagingSenderId: "959466756780",
+  appId: "1:959466756780:web:2bac65987af1d650d49d09",
+};
+
+const app = initializeApp(firebaseConfig);
