e4e1b1c613aa9db9efa642df9b637f7406022f45
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-03-05 11:56:20 +0530

    rs-media made

--- chat-app/src/components/RightSidebar/RightSidebar.jsx
+++ chat-app/src/components/RightSidebar/RightSidebar.jsx
@@ -8,8 +8,21 @@ const RightSidebar = () => {
       <div className="rs-profile">
         <img src={assets.profile_img} alt="" />
         <h3>Ramesh Pawan<img src={assets.green_dot} className='dot' alt=''/></h3>
-        <p>Hey, I like you so mush  </p>
-      </div>    
+        <p>Hey, I am using this templates</p>
+      </div>
+      <hr />
+      <div className="rs-media">
+        <p>Media</p>
+        <div>
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+          <img src={assets.pic3} alt="" />
+          <img src={assets.pic4} alt="" />
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+        </div>
+      </div>
+      <button>Logout</button>    
     </div>
   )
 }
