46e880255f650acba3b0701f6f9c8b1164c3cfdb
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-03-05 14:06:11 +0530

    profile class add profileupdate.jsx file

--- chat-app/src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ chat-app/src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import './ProfileUpdate.css'
+import assets from '../../assets/assets'
 
 const ProfileUpdate = () => {
   return (
-    <div>
-        we are the profile page 
+    <div className='profile'>
+        <div className="profile-container">
+          <form action="">
+            <h3>Profile Details</h3>
+            <label htmlFor="avatar">
+              <input type="text" id='avatar' accept='.png, .jpg, .jpeg' hidden/>
+              <img src={assets.avatar_icon} alt="" />
+              upload profile image
+            </label>
+            <input type="text"  placeholder='Your Name' required/>
+            <textarea  placeholder='Write profile bio' required></textarea>
+            <button type='submit'>save</button>
+          </form>
+          <img src={assets.logo_icon} alt="" />
+        </div>
     </div>
   )
 }
