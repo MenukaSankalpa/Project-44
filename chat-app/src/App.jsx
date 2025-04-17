d9821c39f1aca9fe19ff087f1dadc96f8acf63a1
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-04-17 11:35:33 +0530

    updated app.jsx file

--- chat-app/src/App.jsx
+++ chat-app/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Login from './pages/Login/Login'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Chat from './pages/Chat/Chat'
+import Login from './pages/Login/Login'
 import ProfileUpdate from './pages/ProfileUpdate/ProfileUpdate'
 
 const App = () => {
   return (
     <>
+    <ToastContainer/>
       <Routes>
         <Route path='/' element={<Login/>}/>
         <Route path='/chat' element={<Chat/>}/>
