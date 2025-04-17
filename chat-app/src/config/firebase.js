913cc6ccd3cdba1e0c8f0bfcc37c57b24b1d9111
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-04-17 11:31:09 +0530

    signup function made

--- chat-app/src/config/firebase.js
+++ chat-app/src/config/firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { getFirestore, setDoc } from "firebase/firestore";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyCRSvq_NOfPeg3JgmFhGCPT7_abpT1gCRs",
@@ -10,4 +12,29 @@ const firebaseConfig = {
   appId: "1:959466756780:web:2bac65987af1d650d49d09",
 };
 
+//Initialize Firebase 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (username,email,password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth,email,password);
+    const user = res.user;
+    //to save this data for firestone database 
+    await setDoc(doc(db, "users",user.uid),{
+      id:user.uid,
+      username:username.toLowerCase(),
+      email,
+      name:"",
+      avatar:"",
+      bio:"Hey, There i am using chat app",
+      lastSeen:Date.now()
+    })
+    await setDoc(doc(db, "chats", user.id), {
+      chatData:[]
+    })
+  } catch (error) {
+    
+  }
+}
